9ce0a3f09199e40683457a96fb2c73ca1c73cab3
akshayThool <akshay.thool@gmail.com>
2022-06-06 18:58:12 +0530

    Added time duration to calculate time taken

--- PrimaryDataTypes/NosOfBitsProblem-5.1/lookUpMain.cpp
+++ PrimaryDataTypes/NosOfBitsProblem-5.1/lookUpMain.cpp
@@ -1,8 +1,10 @@
 #include "brute-force.h"
 #include<iostream>
 #include<cmath>
+#include<chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int main(){
 
@@ -12,14 +14,19 @@ int main(){
    int numberOfLookUps = totalBits/lookUpBits;
    int parityArray[sizeOfParityArray];
 
+    auto startTime = high_resolution_clock::now();
    for(int i = 0; i < sizeOfParityArray; i++){
        parityArray[i] = lowestBitMethod(i);
    }
+    auto endTime = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(endTime - startTime);
 
-   long long number = 150119987579016;
+    cout<<"Total time to create look up table "<<duration.count()<<"\n";
+
+   long long number = 562949953421311;
    long long bitmask = sizeOfParityArray - 1;
    int parity = 0;
-
+    startTime = high_resolution_clock::now();
    for(int i = 0; i < numberOfLookUps; i++){
        int lookUpNumber = number & bitmask;
        if(parityArray[lookUpNumber]){
@@ -27,7 +34,10 @@ int main(){
        }
        number >>= lookUpBits;
    }
+endTime = high_resolution_clock::now();
+duration = duration_cast<microseconds>(endTime - startTime);
 
-   cout<<"Parity is "<<parity<<endl;
+
+   cout<<"Parity is "<<parity<<" and time taken is "<<duration.count()<<"\n";
 
 }
