6a2f9ed9dce07df82c9e10de879190c9a2dedebc
Akshay Thool <akshay.thool@gmail.com>
2022-06-18 20:32:39 +0530

    Division 5.6 - added useful comment and git ignore file

--- PrimaryDataTypes/Division-5.6/main.cpp
+++ PrimaryDataTypes/Division-5.6/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+    // Number 4294967295 represents Binary Number 11111111111111111111111111111111
      cout<<divisionBF(4294967295, 1)<<"\n";
     cout<<division2k(4294967295, 1)<<"\n";
     cout<<divisionLargestFirst(4294967295, 1)<<"\n";
