37318a3179396deeb4cdc9fc9db59ab26bd89515
Akshay Thool <akshay.thool@gmail.com>
2022-06-18 10:05:24 +0530

    Added O(n) approach with nosOfIteration calculation

--- PrimaryDataTypes/Division-5.6/bitCalc.h
+++ PrimaryDataTypes/Division-5.6/bitCalc.h
@@ -1,8 +1,10 @@
 #ifndef BITCALC_H
 #define BITCALC_H
+#include<iostream>
 
 
 unsigned divisionBF(unsigned dividend, unsigned divisor);
 unsigned division2k(unsigned dividend, unsigned divisor);
+unsigned divisionLargestFirst(unsigned dividend, unsigned divisor);
 
 #endif // BITCALC_H
